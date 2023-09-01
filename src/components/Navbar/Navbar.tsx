1f2b629c949f09a89479bd0648d09583c180426e
ikomaz97 <ikomaz97@gmail.com>
2023-09-01 18:33:46 +0400

    01.09.2023-2

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import s from './Navbar.module.css'
 
-// let s ={
-//     'nav': 'Navbar_nav__LK3K4',
-//     'item': 'Navbar_item__4MjPa',
-// }
 
-// let c1 = 'item'
-// let c2 = 'active'
-//
-// let classesNew = `${c1} ${c2}`;
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a>Profile</a>
+                <a href='/Profile'>Profile</a>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a>Message</a>
+                <a href='/Message' >Message</a>
             </div>
             <div className={s.item}>
                 <a>News</a>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,9 +4,10 @@ import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
              <img   src='https://c4.wallpaperflare.com/wallpaper/818/26/537/video-games-video-game-art-ultrawide-ultra-wide-god-of-war-hd-wallpaper-preview.jpg'/>
+                alt='Игровое искусство - Бог войны'
             </div>
             <div>
               ava + description
