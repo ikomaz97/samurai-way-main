f9f8f51c49f3a255567a07bda8ed93674966d02d
ikomaz97 <ikomaz97@gmail.com>
2023-09-01 19:06:32 +0400

    01.09.2023-77

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,13 +13,13 @@ const Navbar = () => {
                 <a href='/Message' >Message</a>
             </div>
             <div className={s.item}>
-                <a>News</a>
+                <a href='/News' >News</a>
             </div>
             <div className={s.item}>
-                <a>Music</a>
+                <a href='/Music' >Music</a>
             </div>
             <div className={s.item}>
-                <a>Settings</a>
+                <a href='/Settings' >Settings</a>
             </div>
         </nav>
     );
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -7,7 +7,6 @@ const Profile = () => {
         <div>
             <div>
              <img   src='https://c4.wallpaperflare.com/wallpaper/818/26/537/video-games-video-game-art-ultrawide-ultra-wide-god-of-war-hd-wallpaper-preview.jpg'/>
-                alt='Игровое искусство - Бог войны'
             </div>
             <div>
               ava + description
