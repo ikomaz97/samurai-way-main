14d6a27ef8839fef85d7ded4c7f6887e2400e455
ikomaz97 <ikomaz97@gmail.com>
2023-09-06 10:18:52 +0400

    05.09.2023-3

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './ProfileInfo.module.css';
+
+
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img src='https://c4.wallpaperflare.com/wallpaper/818/26/537/video-games-video-game-art-ultrawide-ultra-wide-god-of-war-hd-wallpaper-preview.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+        </div>
+    );
+};
+
+export default ProfileInfo;
