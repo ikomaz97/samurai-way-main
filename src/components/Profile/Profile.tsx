fb8b6d1aeb307a3b38b9e9cf4ae09cb4bdffc418
ikomaz97 <ikomaz97@gmail.com>
2023-09-06 10:19:02 +0400

    05.09.2023-4

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts'
-import Dialogs from "../Dialogs/Dialogs";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+
+
 
 const Profile = () => {
     return (
         <div>
-            <div>
-             <img   src='https://c4.wallpaperflare.com/wallpaper/818/26/537/video-games-video-game-art-ultrawide-ultra-wide-god-of-war-hd-wallpaper-preview.jpg'/>
-            </div>
-            <div>
-              ava + description
-            </div>
+            <ProfileInfo/>
             <MyPosts/>
-       </div>
+        </div>
     );
 };
 
