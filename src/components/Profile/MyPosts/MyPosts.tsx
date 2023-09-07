7d822ec39e65e76cf960328fed3e2dca3bea9604
ikomaz97 <ikomaz97@gmail.com>
2023-09-07 10:01:44 +0400

    07.09.2023-3

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,17 +2,29 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props:any) => {
+
+    let posts = [
+        {id:1, message:'Hi, how are you?', likeCount: 12},
+        {id:2, message:'How is you IT - kamasutra?', likeCount: 11 },
+    ]
+
+    let postElement = posts.map((p)=>{
+        <Post message = {p.message} likesCount = {p.likeCount} />
+    })
     return (
-            <div>
-                My posts
+            <div className={s.postsBlock}>
+                <h3>My posts</h3>
                 <div>
+                    <div>
                     <textarea></textarea>
+                    </div>
+                    <div>
                     <button>Add post</button>
+                    </div>
                 </div>
                 <div className={s.posts}>
-                   <Post message = 'Hi, how are you?' likesCount = "0" />
-                   <Post message = 'Hi, how are you?' likesCount = "23"/>
+                    {postElement}
                 </div>
             </div>
     );
