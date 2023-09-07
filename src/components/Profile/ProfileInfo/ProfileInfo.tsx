6d62cdb9ddf5edbad3137865f15fc54fd410b036
ikomaz97 <ikomaz97@gmail.com>
2023-09-07 10:02:14 +0400

    07.09.2023-5

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -9,7 +9,7 @@ const ProfileInfo = () => {
             <div>
                 <img src='https://c4.wallpaperflare.com/wallpaper/818/26/537/video-games-video-game-art-ultrawide-ultra-wide-god-of-war-hd-wallpaper-preview.jpg'/>
             </div>
-            <div>
+            <div className={s.descriptionBlock}>
                 ava + description
             </div>
         </div>
