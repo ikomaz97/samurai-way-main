b7158a7c83dd38caea3e7773f63c906f4c979416
ikomaz97 <ikomaz97@gmail.com>
2023-09-12 14:40:38 +0400

    12.09.2023-1

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,25 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import Post from "./components/Profile/MyPosts/Post/Post";
+import MyPosts from "./components/Profile/MyPosts/MyPosts";
+
+function App(props:any) {
 
 
-function App() {
+
     return (
+
         <BrowserRouter>
             <div className="app-wrapper">
                 <Header/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    <Route path='/message/'  component={Dialogs}/>
-                    <Route path='/profile/'  component={Profile}/>
+                    {/*<Route path='/message/'  component={Dialogs}/>*/}
+                    {/*<Route path='/profile/'  component={Profile}/>*/}
+
+                    <Route path='/message/'  render={()=><Dialogs/>}/>
+                    <Route path='/profile/'  render={()=><Profile posts ={props.posts}/>}/>
                 </div>
             </div>
         </BrowserRouter>
