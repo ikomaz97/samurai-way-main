10e8d0dab585453e519835633e84dd07fb0c274c
ikomaz97 <ikomaz97@gmail.com>
2023-09-12 14:40:57 +0400

    12.09.2023-3

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+let posts = [
+    {id:1, message:'Hi, how are you?', likeCount: 12},
+    {id:2, message:'How is you IT - kamasutra?', likeCount: 14 },
+    {id:3, message:'How is you IT - kamasutra?', likeCount: 10 },
+    {id:4, message:'How is you IT - kamasutra?', likeCount: 11 },
+]
+
+
 ReactDOM.render(
-    <App />,
+    <App
+    posts = {posts}
+    />,
   document.getElementById('root')
 );
