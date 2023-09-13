5e81fa9595e940e2cf28cd61b9928862cd19fb10
ikomaz97 <ikomaz97@gmail.com>
2023-09-13 13:13:04 +0400

    13.09.2023-2

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-let posts = [
-    {id:1, message:'Hi, how are you?', likeCount: 12},
-    {id:2, message:'How is you IT - kamasutra?', likeCount: 14 },
-    {id:3, message:'How is you IT - kamasutra?', likeCount: 10 },
-    {id:4, message:'How is you IT - kamasutra?', likeCount: 11 },
-]
+import state from "./components/redux/state";
 
 
 ReactDOM.render(
-    <App
-    posts = {posts}
-    />,
+    <App state = {state}/>,
   document.getElementById('root')
 );
