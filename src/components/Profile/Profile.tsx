13f570e9730caa6f2674e96dd61823999fa3125f
ikomaz97 <ikomaz97@gmail.com>
2023-09-14 20:09:53 +0400

    14.09.2023-2

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts'
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import addPost from "../../redux/state";
 
 
 
@@ -10,7 +11,10 @@ const Profile = (props:any) => {
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts posts={props.state.posts}/>
+            <MyPosts posts={props.profilePage.posts}
+                     newPostText={props.profilePage.newPostText}
+                     updateNewPostText={props.updateNewPostText}
+                     addpost = {addPost}/>
         </div>
     );
 };
