59bf3c2e27e3f6f24f2c412a7d99d0313aac7b77
ikomaz97 <ikomaz97@gmail.com>
2023-09-14 20:10:08 +0400

    14.09.2023-0

---
+++ src/redux/state.tsx
@@ -0,0 +1,82 @@
+import { renderEntireFree } from "../render";
+
+interface Post {
+    id: number;
+    message: string;
+    likeCount: number;
+}
+
+interface Dialog {
+    id: number;
+    name: string;
+}
+
+interface Message {
+    id: number;
+    message: string;
+}
+
+interface ProfilePage {
+    posts: Post[];
+    newPostText: string;
+}
+
+interface DialogsPage {
+    dialogs: Dialog[];
+    messages: Message[];
+    sidebar: {};
+}
+
+interface State {
+    profilePage: ProfilePage;
+    dialogsPage: DialogsPage;
+}
+
+let state: State = {
+    profilePage: {
+        posts: [
+            { id: 1, message: 'Hi, how are you?', likeCount: 12 },
+            { id: 2, message: 'How is your IT - kamasutra?', likeCount: 14 },
+            { id: 3, message: 'How is your IT - kamasutra?', likeCount: 10 },
+            { id: 4, message: 'How is your IT - kamasutra?', likeCount: 11 },
+        ],
+        newPostText: "IT-COMASUTRA.COM",
+    },
+    dialogsPage: {
+        dialogs: [
+            { id: 1, name: 'Dimych' },
+            { id: 2, name: 'Andrey' },
+            { id: 3, name: 'Sveta' },
+            { id: 4, name: 'Sasha' },
+            { id: 5, name: 'Victor' },
+            { id: 6, name: 'Valera' },
+        ],
+        messages: [
+            { id: 1, message: 'Hi' },
+            { id: 2, message: 'How is your IT - kamasutra?' },
+            { id: 3, message: 'Yo' },
+            { id: 4, message: 'Yo' },
+            { id: 5, message: 'Yo' },
+            { id: 6, message: 'Yo' },
+        ],
+        sidebar: {},
+    },
+};
+
+export const addPost = () => {
+    let newPost: Post = {
+        id: state.profilePage.posts.length + 1,
+        message: state.profilePage.newPostText,
+        likeCount: 0,
+    };
+    state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = "";
+    renderEntireFree(state);
+};
+
+export const updateNewPostText = (newText: string) => {
+    state.profilePage.newPostText = newText;
+    renderEntireFree(state);
+};
+
+export default state;
