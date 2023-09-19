e7c86fe0e905dd95aecc1bbf7c18a898307f101b
ikomaz97 <ikomaz97@gmail.com>
2023-09-19 16:18:50 +0400

    19.09.2023-0

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDom from 'react-dom';
 import './index.css';
-import state from "./redux/state";
-import {renderEntireFree} from "./render";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import state, {updateNewPostText, addPost, subscriber} from './redux/state';
+import { BrowserRouter } from 'react-router-dom';
+ const renderEntireFree = (appState: typeof state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={appState}
+                 updateNewPostText={updateNewPostText}
+                 addpost={addPost} />
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+};
+
 
 renderEntireFree(state);
+subscriber(renderEntireFree);
 
