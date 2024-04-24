22c9fc26859eb8208b0bb45c2aad0d1cabd7c16d
ikomaz97 <ikomaz97@gmail.com>
2024-04-24 18:51:19 +0400

    24.04.21-

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -30,61 +30,59 @@ interface State {
     dialogsPage: DialogsPage;
 }
 
-let renderEntireTree: (state: State) => void = (state) => {
-    console.log("State changed:", state);
-};
-
-let state: State = {
-    profilePage: {
-        posts: [
-            { id: 1, message: "Hi, how are you?", likeCount: 12 },
-            { id: 2, message: "How is your IT - kamasutra?", likeCount: 14 },
-            { id: 3, message: "How is your IT - kamasutra?", likeCount: 10 },
-            { id: 4, message: "How is your IT - kamasutra?", likeCount: 11 },
-        ],
-        newPostText: "IT-COMASUTRA.COM",
+let store = {
+    _state:  {
+        profilePage: {
+            posts: [
+                { id: 1, message: "Hi, how are you?", likeCount: 12 },
+                { id: 2, message: "How is your IT - kamasutra?", likeCount: 14 },
+                { id: 3, message: "How is your IT - kamasutra?", likeCount: 10 },
+                { id: 4, message: "How is your IT - kamasutra?", likeCount: 11 },
+            ],
+            newPostText: "IT-COMASUTRA.COM",
+        },
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: "Dimych" },
+                { id: 2, name: "Andrey" },
+                { id: 3, name: "Sveta" },
+                { id: 4, name: "Sasha" },
+                { id: 5, name: "Victor" },
+                { id: 6, name: "Valera" },
+            ],
+            messages: [
+                { id: 1, message: "Hi" },
+                { id: 2, message: "How is your IT - kamasutra?" },
+                { id: 3, message: "Yo" },
+                { id: 4, message: "Yo" },
+                { id: 5, message: "Yo" },
+                { id: 6, message: "Yo" },
+            ],
+            sidebar: {},
+        },
     },
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: "Dimych" },
-            { id: 2, name: "Andrey" },
-            { id: 3, name: "Sveta" },
-            { id: 4, name: "Sasha" },
-            { id: 5, name: "Victor" },
-            { id: 6, name: "Valera" },
-        ],
-        messages: [
-            { id: 1, message: "Hi" },
-            { id: 2, message: "How is your IT - kamasutra?" },
-            { id: 3, message: "Yo" },
-            { id: 4, message: "Yo" },
-            { id: 5, message: "Yo" },
-            { id: 6, message: "Yo" },
-        ],
-        sidebar: {},
+    renderEntireTree ()   {
+        console.log("State changed:", state);
     },
+    addPost ()  {
+        let newPost: Post = {
+            id: state.profilePage.posts.length + 1,
+            message: state.profilePage.newPostText,
+            likeCount: 0,
+        };
+        state.profilePage.posts.push(newPost);
+        state.profilePage.newPostText = "";
+        renderEntireTree(state);
+    },
+    updateNewPostText (newText)  {
+        state.profilePage.newPostText = newText;
+        renderEntireTree(state);
+    },
+    subscriber (observer)    {
+        renderEntireTree = observer;
+    },
+
 };
 
-export const addPost: () => void = () => {
-    let newPost: Post = {
-        id: state.profilePage.posts.length + 1,
-        message: state.profilePage.newPostText,
-        likeCount: 0,
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = "";
-    renderEntireTree(state);
-};
-
-export const updateNewPostText: (newText: string) => void = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-};
-
-export const subscriber: (observer: (state: State) => void) => void = (
-    observer
-) => {
-    renderEntireTree = observer;
-};
-
-export default state;
+export default store;
+window.store = store;
