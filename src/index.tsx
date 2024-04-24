7595d16fea05fa89e277459eea113d7d22a7704c
ikomaz97 <ikomaz97@gmail.com>
2024-04-24 18:58:25 +0400

    24.04.21-2

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,13 @@ import './index.css';
 import App from './App';
 import state, {updateNewPostText, addPost, subscriber,} from './redux/state';
 import {BrowserRouter} from 'react-router-dom';
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+
+
 
 let renderEntireFree = (appState: typeof state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <DevSupport ComponentPreviews={ComponentPreviews}
-                        useInitialHook={useInitial}
-            >
+
                 <App state={appState}
                      updateNewPostText={updateNewPostText}
                      addpost={addPost}/>
