39e3efc5d2feb25741f117733adb5b5616539651
ikomaz97 <ikomaz97@gmail.com>
2024-06-06 17:40:20 +0400

    30.04.21-4

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,27 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
-function App(props: any) {
+import { BrowserRouter, Route } from "react-router-dom";
 
-
+const App = (props: any) => {
     return (
-
         <BrowserRouter>
             <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
+                <Header />
+                <Navbar />
                 <div className="app-wrapper-content">
-                    <Route path='/message'
-                           render={() => <Dialogs  state={props.state.dialogsPage}/>}/>
-                    <Route path='/profile'
-                           render={() => <Profile profilePage={props.state.profilePage}
-                                                  ProfilePage ={props.state.profilePage}
-                                                  addPost = {props.addPost}
-                                                  updateNewPostText={props.updateNewPostText}
-                           />}/>
+                    <Route path='/message' render={() => <Dialogs state={props.state.dialogsPage} />} />
+                    <Route path='/profile' render={() => (
+                        <Profile
+                            profilePage={props.state.profilePage}
+                            addPost={props.addPost}
+                            updateNewPostText={props.updateNewPostText}
+                        />
+                    )} />
                 </div>
             </div>
         </BrowserRouter>
     );
-}
+};
 
 export default App;
