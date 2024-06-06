dd03df006605f6729df7214f92f449f49846697f
ikomaz97 <ikomaz97@gmail.com>
2024-06-06 17:40:46 +0400

    30.04.21-6

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,41 +1,52 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
+type PostType = {
+    id: number;
+    message: string;
+    likeCount: number;
+};
 
+type MyPostsProps = {
+    posts: PostType[];
+    newPostText: string;
+    addPost: () => void;
+    updateNewPostText: (text: string) => void;
+};
 
-const MyPosts = (props: any) => {
+const MyPosts: React.FC<MyPostsProps> = (props) => {
+    const postElements = props.posts.map((p) => (
+        <Post key={p.id} message={p.message} likesCount={p.likeCount} />
+    ));
 
-    const postElement = props.posts.map((p: any) =>
-        <Post message={p.message} likesCount={p.likeCount}/>
-    )
-    const newPostsElement = React.createRef<HTMLTextAreaElement>
-    () as React.RefObject<HTMLTextAreaElement>;
+    const newPostElement = React.createRef<HTMLTextAreaElement>();
+
     const addPost = () => {
         props.addPost();
-
     };
 
-    const onPostChange = () => {
-        if (newPostsElement.current !== null) {
-            let text = newPostsElement.current.value as string;
-            props.updateNewPostText(text);
-        }
-    }
+    const onPostChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        props.updateNewPostText(event.target.value);
+    };
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea onChange={onPostChange} ref={newPostsElement} value={props.newPostText}/>
+          <textarea
+              onChange={onPostChange}
+              ref={newPostElement}
+              value={props.newPostText}
+          />
                 </div>
                 <div>
                     <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
-                {postElement}
+                {postElements}
             </div>
         </div>
     );
